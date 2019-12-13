6676e6b8fc879eb9308837513beff114e9d5a16a
Ben Anderson <dataknut@icloud.com>
2019-12-13 17:07:08 +0000

    re-wrote slightly

--- installMe.R
+++ installMe.R
@@ -1,20 +1,55 @@
 # Script to run after updating R
+
+installMyPackages <- function(..., repository = "https://cran.rstudio.com/"){
+  
+  packages <- c(...)
+  
+  # Find if package isn't installed
+  newPackages <- packages[!(packages %in% utils::installed.packages()[,1])]
+  
+  # Install if required
+  
+  if (length(newPackages)){
+    message("Installing: ", newPackages)
+    utils::install.packages(newPackages,
+                            dependencies = TRUE)
+  } else {
+    message("None missing")
+  }
+}
+
+myPacks <- c("bookdown",
+             "car",
+             "data.table", 
+             "devtools",
+             "drake", 
+             "ggplot2", 
+             "ggspatial",
+             "here", 
+             "Hmisc",
+             "hms", 
+             "ipfp",
+             "kableExtra", 
+             "knitr", 
+             "leaflet",
+             "lubridate", 
+             "plotly", 
+             "rmarkdown",
+             "roxygen2",
+             "sf",
+             "skimr",
+             "tidyverse",
+             "usethis"
+)
+
+# do this manually
 update.packages()
 
-install.packages(c("data.table", 
-                  "drake", 
-                  "ggplot2", 
-                  "ggspatial",
-                  "here", 
-                  "Hmisc",
-                  "hms", 
-                  "ipfp",
-                  "kableExtra", 
-                  "knitr", 
-                  "leaflet",
-                  "lubridate", 
-                  "plotly", 
-                  "sf",
-                  "skimr",
-                  "tidyverse")
-                 )
+# only install the ones that are missing
+installMyPackages(myPacks)
+
+# get github ones
+devtools::install_github("dataknut/dkUtils")
+devtools::install_github("CfSOtago/GREENGridData")
+
+# then re-build local packages if needed
