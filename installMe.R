4bbb87d803ddbeddb4e343bc66d44efd42efa80a
Ben Anderson <dataknut@icloud.com>
2019-12-13 14:41:09 +0000

    installs most used packages

---
+++ installMe.R
@@ -0,0 +1,18 @@
+# Script to run after updating R
+
+install.packages(c("data.table", 
+                  "drake", 
+                  "ggplot2", 
+                  "ggspatial",
+                  "here", 
+                  "Hmisc",
+                  "hms", 
+                  "kableExtra", 
+                  "knitr", 
+                  "leaflet",
+                  "lubridate", 
+                  "plotly", 
+                  "sf",
+                  "skimr",
+                  "tidyverse")
+                 )
