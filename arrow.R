02200e83f3ec275025a8bcbd48cd55f14c1dc1b3
Ben Anderson <dataknut@icloud.com>
2022-07-01 14:29:01 +0100

    added arrow test

---
+++ arrow.R
@@ -0,0 +1,62 @@
+# https://arrow-user2022.netlify.app/hello-arrow.html
+
+# Time flies like an arrow, fruit flies like a banana
+# do we really need all these loaded?
+libs <- c("arrow",
+          "dplyr",
+          "lubridate",
+          "duckdb",
+          "stringr",
+          "palmerpenguins",
+          "tictoc",
+          "scales",
+          "janitor",
+          "fs",
+          "ggplot2",
+          "ggrepel",
+          "sf")
+
+dkUtils::loadLibraries(libs)
+
+path <- "~/Dropbox/data/SAVE/UKDA-SN-8676-1/save_consumption_data/"
+
+save_Wh <- arrow::open_dataset(path, format = "csv")
+
+save_Wh %>%
+  head() %>%
+  collect()
+
+# Energy = cumulative Wh within bmg_id - you'd forgotten, right? lol
+dkUtils::tidyNum(nrow(save_Wh), round = 1)
+
+tic()
+# create an arrow table object with just 2018 data
+save_2018_Wh <- save_Wh %>%
+  mutate( dateTime = cast(recorded_timestamp, timestamp()),
+          year = year(dateTime)) %>%
+  filter(year == 2018) %>%
+  compute()
+toc()
+
+dkUtils::tidyNum(nrow(save_2018_Wh), round = 1)
+
+tic()
+save_2018_Wh <- save_2018_Wh %>%
+  group_by(bmg_id) %>% # do we need to 'arrange' by dateTime here to be sure?
+  mutate(wh = energy - lag(energy)) %>% # pulls into R so subsequent memory issues
+  compute()
+toc()
+
+save_2018_Wh %>%
+  head() %>%
+  collect()
+
+t <- save_2018_Wh %>%
+  mutate(date = date(dateTime)) %>%
+  group_by(dateTime) %>%
+  summarise(meanEnergy = mean(wh/1000),
+            medianEnergy = median(wh/1000)) %>%
+  collect()
+
+ggplot2::ggplot(t, aes(x = date, y = medianEnergy)) +
+  geom_point()
