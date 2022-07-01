11eb7255d73ec3d6d52b7d4dc0ff66970e17ab6d
Ben Anderson <dataknut@icloud.com>
2022-07-01 17:14:56 +0100

    updated arrow example

--- arrow.R
+++ arrow.R
@@ -18,6 +18,9 @@ libs <- c("arrow",
 
 dkUtils::loadLibraries(libs)
 
+# Test on SAVE electricity use data (large dataset)
+# https://beta.ukdataservice.ac.uk/datacatalogue/studies/study?id=8676
+
 path <- "~/Dropbox/data/SAVE/UKDA-SN-8676-1/save_consumption_data/"
 
 save_Wh <- arrow::open_dataset(path, format = "csv")
@@ -43,6 +46,7 @@ dkUtils::tidyNum(nrow(save_2018_Wh), round = 1)
 tic()
 save_2018_Wh <- save_2018_Wh %>%
   group_by(bmg_id) %>% # do we need to 'arrange' by dateTime here to be sure?
+  arrange(bmg_id, dateTime) %>%
   mutate(wh = energy - lag(energy)) %>% # pulls into R so subsequent memory issues
   compute()
 toc()
@@ -51,10 +55,12 @@ save_2018_Wh %>%
   head() %>%
   collect()
 
+summary(save_2018_Wh)
+
 t <- save_2018_Wh %>%
   mutate(date = date(dateTime)) %>%
-  group_by(dateTime) %>%
-  summarise(meanEnergy = mean(wh/1000),
+  group_by(date) %>%
+  summarise(meanEnergy = mean(wh/1000, na.rm = TRUE),
             medianEnergy = median(wh/1000)) %>%
   collect()
 
