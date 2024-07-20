18f72c8089875b4e351735f278c927bfd54a4cc4
Ben Anderson <dataknut@icloud.com>
2024-07-20 13:00:36 +1200

    initial zoo/xts play

---
+++ timeseries.R
@@ -0,0 +1,25 @@
+# zoo and xts
+
+library(xts)
+
+lastweek <- last(temps, "1 week")
+
+data(sunspots)
+class(sunspots)
+
+# Convert sunspots to xts using as.xts().
+sunspots_xts <- as.xts(sunspots)
+
+# Get the temporary file name
+tmp <- tempfile()
+
+# Write the xts object using zoo to tmp 
+write.zoo(sunspots_xts, sep = ",", file = tmp)
+
+# Read the tmp file. FUN = as.yearmon converts strings such as Jan 1749 into a proper time class
+sun <- read.zoo(tmp, sep = ",", FUN = as.yearmon)
+
+# Convert sun into xts. Save this as sun_xts
+sun_xts <- as.xts(sun)
+
+periodicity(sun_xts)
